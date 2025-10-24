1e6d15c7941fb9580528d528016a7f246a174e91
ArneSchulteHuermann <arne@schulte-huermann.com>
2025-10-24 15:07:51 +0200

    My first website

--- app.py
+++ app.py
@@ -3,25 +3,11 @@ from datetime import datetime
 import requests
 
 '''
-# TaxiFareModel front
+# TaxiFareModel
 '''
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
 '''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
+## Add your variables in order to receive a price estimation
 '''
 pickup_date = st.date_input("Pickup date")
 pickup_time = st.time_input("Pickup time")
@@ -35,20 +21,8 @@ passenger_count = st.number_input("Passenger count", step=1)
 # --- Combine date + time ---
 pickup_datetime = datetime.combine(pickup_date, pickup_time).isoformat()
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
 
 url = 'https://taxifare.lewagon.ai/predict'
-
-if url == 'https://taxifare.lewagon.ai/predict':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
 
 params = dict(
     pickup_datetime=pickup_datetime,
@@ -61,15 +35,6 @@ params = dict(
 wagon_cab_api_url = 'https://taxifare.lewagon.ai/predict'
 response = requests.get(wagon_cab_api_url, params=params)
 
-st.write(response.json())
 
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
+prediction = response.json().get("fare", "No fare found")
+st.success(f"🚕 Estimated Fare: **${prediction:.2f}**")
